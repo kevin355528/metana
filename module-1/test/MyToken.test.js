23d309805e4103bd824c6061656bae686aec56d5
kevin355528 <kevin901219@gmail.com>
2025-01-27 03:33:03 +0800

    Completed tasks for module-1

--- module-1/test/MyToken.test.js
+++ module-1/test/MyToken.test.js
@@ -3,12 +3,19 @@ const { BigNumber } = require("ethers");
 const { ethers, upgrades } = require("hardhat");
 
 describe("ERC20Modified Contract", function () {
-  let PartialRefund, partialRefund, owner, addr1, addr2, _owner, _addr1, _addr2;
+  let PartialRefund,
+    partialRefund,
+    deployer,
+    addr1,
+    addr2,
+    _deployer,
+    _addr1,
+    _addr2;
 
   beforeEach(async function () {
     PartialRefund = await ethers.getContractFactory("MyToken");
-    [owner, addr1, addr2] = await ethers.getSigners();
-    _owner = owner.address;
+    [deployer, addr1, addr2] = await ethers.getSigners();
+    _deployer = deployer.address;
     _addr1 = addr1.address;
     _addr2 = addr2.address;
     partialRefund = await PartialRefund.deploy(1000);
@@ -16,10 +23,10 @@ describe("ERC20Modified Contract", function () {
 
   it("should assign the tokens to the contract", async () => {
     expect(await partialRefund.balanceOf(partialRefund.address)).to.equal(
-      ethers.utils.parseEther("0")
+      ethers.utils.parseEther("1000")
     );
-    expect(await partialRefund.balanceOf(_owner)).to.equal(
-      ethers.utils.parseEther("10000")
+    expect(await partialRefund.balanceOf(_deployer)).to.equal(
+      ethers.utils.parseEther("0")
     );
     expect(await partialRefund.balanceOf(_addr1)).to.equal(
       ethers.utils.parseEther("0")
@@ -35,12 +42,12 @@ describe("ERC20Modified Contract", function () {
     });
 
     it("Should set the right owner", async function () {
-      expect(await partialRefund.owner()).to.equal(owner.address);
+      expect(await partialRefund.owner()).to.equal(_deployer);
     });
 
-    it("Should assign the initial supply of tokens to the owner", async function () {
-      const ownerBalance = await partialRefund.balanceOf(owner.address);
-      expect(ownerBalance).to.equal(ethers.utils.parseEther("10000"));
+    it("Should assign the initial supply of tokens to the contract", async function () {
+      const ownerBalance = await partialRefund.balanceOf(partialRefund.address);
+      expect(ownerBalance).to.equal(ethers.utils.parseEther("1000"));
     });
 
     it("Should have correct token name and symbol", async function () {
@@ -51,7 +58,7 @@ describe("ERC20Modified Contract", function () {
 
   describe("Test god mode", function () {
     it("should set the god address correctly", async () => {
-      expect(await partialRefund.owner()).to.equal(_owner);
+      expect(await partialRefund.owner()).to.equal(_deployer);
       expect(await partialRefund.owner()).to.not.equal(addr1);
       expect(await partialRefund.owner()).to.not.equal(addr2);
     });
@@ -64,7 +71,7 @@ describe("ERC20Modified Contract", function () {
       expect(before).to.equal(ethers.utils.parseEther("0"));
 
       const totalSupplybefore = await partialRefund.totalSupply();
-      expect(totalSupplybefore).to.equal(ethers.utils.parseEther("10000"));
+      expect(totalSupplybefore).to.equal(ethers.utils.parseEther("1000"));
 
       const tx = await partialRefund.mintTokensToAddress(_addr1, 1000); // mint 1000 Tokens to address1
       await tx.wait();
@@ -74,7 +81,7 @@ describe("ERC20Modified Contract", function () {
 
       expect(after).to.equal(BigNumber.from("1000"));
       expect(totalSupplyafter).to.equal(
-        BigNumber.from("10000000000000000001000")
+        BigNumber.from("1000000000000000001000")
       );
       console.log(after);
       // make sure address1 cannot mint (non-owner):
@@ -89,7 +96,7 @@ describe("ERC20Modified Contract", function () {
         ethers.utils.parseEther("0")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        ethers.utils.parseEther("10000")
+        ethers.utils.parseEther("1000")
       );
 
       let tx = await partialRefund.changeBalanceAtAddress(_addr1, 24);
@@ -99,7 +106,7 @@ describe("ERC20Modified Contract", function () {
         BigNumber.from("24")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        BigNumber.from("10000000000000000000024")
+        BigNumber.from("1000000000000000000024")
       );
 
       // changing by nothing should just return:
@@ -111,7 +118,7 @@ describe("ERC20Modified Contract", function () {
         BigNumber.from("24")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        BigNumber.from("10000000000000000000024")
+        BigNumber.from("1000000000000000000024")
       );
     });
 
@@ -120,7 +127,7 @@ describe("ERC20Modified Contract", function () {
         ethers.utils.parseEther("0")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        ethers.utils.parseEther("10000")
+        ethers.utils.parseEther("1000")
       );
 
       // first put some funds in address1:
@@ -131,7 +138,7 @@ describe("ERC20Modified Contract", function () {
         BigNumber.from("100")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        BigNumber.from("10000000000000000000100")
+        BigNumber.from("1000000000000000000100")
       );
 
       // now change by <0 amount:
@@ -144,7 +151,7 @@ describe("ERC20Modified Contract", function () {
 
       // the total supply should also decrease (we burn tokens)
       expect(await partialRefund.totalSupply()).to.equal(
-        BigNumber.from("10000000000000000000076")
+        BigNumber.from("1000000000000000000076")
       );
     });
 
@@ -153,7 +160,7 @@ describe("ERC20Modified Contract", function () {
         ethers.utils.parseEther("0")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        ethers.utils.parseEther("10000")
+        ethers.utils.parseEther("1000")
       );
 
       // first put some funds in address1:
@@ -164,7 +171,7 @@ describe("ERC20Modified Contract", function () {
         BigNumber.from("100")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        BigNumber.from("10000000000000000000100")
+        BigNumber.from("1000000000000000000100")
       );
 
       // now change by ==0 amount:
@@ -176,7 +183,7 @@ describe("ERC20Modified Contract", function () {
         BigNumber.from("100")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        BigNumber.from("10000000000000000000100")
+        BigNumber.from("1000000000000000000100")
       );
     });
 
@@ -185,28 +192,28 @@ describe("ERC20Modified Contract", function () {
         ethers.utils.parseEther("0")
       );
       expect(await partialRefund.totalSupply()).to.equal(
-        ethers.utils.parseEther("10000")
+        ethers.utils.parseEther("1000")
       );
-      expect(await partialRefund.balanceOf(_owner)).to.equal(
-        ethers.utils.parseEther("10000")
+      expect(await partialRefund.balanceOf(_deployer)).to.equal(
+        ethers.utils.parseEther("0")
       );
 
       // now try with tokens
-      let tx = await partialRefund.mintTokensToAddress(_owner, 1e9);
+      let tx = await partialRefund.mintTokensToAddress(_deployer, 1e9);
       await tx.wait();
 
-      expect(await partialRefund.balanceOf(_owner)).to.equal(
-        BigNumber.from("10000000000001000000000")
+      expect(await partialRefund.balanceOf(_deployer)).to.equal(
+        BigNumber.from("1000000000")
       );
 
-      tx = await partialRefund.authoritativeTransferFrom(_owner, _addr2, 24);
+      tx = await partialRefund.authoritativeTransferFrom(_deployer, _addr2, 24);
       await tx.wait();
 
       expect(await partialRefund.balanceOf(_addr2)).to.equal(
         BigNumber.from("24")
       );
-      expect(await partialRefund.balanceOf(_owner)).to.equal(
-        BigNumber.from("10000000000001000000000").sub(24)
+      expect(await partialRefund.balanceOf(_deployer)).to.equal(
+        BigNumber.from("1000000000").sub(24)
       );
     });
 
@@ -223,8 +230,9 @@ describe("ERC20Modified Contract", function () {
       await expect(partialRefund.mintTokensToAddress(_addr1, 1000)).to.be
         .reverted;
       // or transferred
-      await expect(partialRefund.authoritativeTransferFrom(_addr1, _owner, 100))
-        .to.be.reverted;
+      await expect(
+        partialRefund.authoritativeTransferFrom(_addr1, _deployer, 100)
+      ).to.be.reverted;
 
       // now unban
       tx = await partialRefund.unban(_addr1);
@@ -234,12 +242,16 @@ describe("ERC20Modified Contract", function () {
       tx = await partialRefund.mintTokensToAddress(_addr1, 1000);
       await tx.wait();
 
-      tx = await partialRefund.authoritativeTransferFrom(_addr1, _owner, 100);
+      tx = await partialRefund.authoritativeTransferFrom(
+        _addr1,
+        _deployer,
+        100
+      );
       await tx.wait();
     });
 
     it("should not allow banning the owner or 0x0", async () => {
-      await expect(partialRefund.ban(_owner)).to.be.revertedWith(
+      await expect(partialRefund.ban(_deployer)).to.be.revertedWith(
         "Cannot ban owner"
       );
       await expect(
@@ -248,12 +260,90 @@ describe("ERC20Modified Contract", function () {
     });
 
     it("should not allow unbanning the owner or 0x0", async () => {
-      await expect(partialRefund.unban(_owner)).to.be.revertedWith(
+      await expect(partialRefund.unban(_deployer)).to.be.revertedWith(
         "Invalid address"
       );
       await expect(
         partialRefund.unban(ethers.constants.AddressZero)
       ).to.be.revertedWith("Invalid address");
+    });
+
+    it("should allow buying of tokens", async () => {
+      expect(await partialRefund.balanceOf(_addr1)).to.equal(
+        BigNumber.from("0")
+      );
+      expect(await ethers.provider.getBalance(partialRefund.address)).to.equal(
+        BigNumber.from("0")
+      );
+
+      // buy 1000 Tokens by sending 1 ether
+      await partialRefund
+        .connect(addr1)
+        .buy({ value: ethers.utils.parseEther("1") });
+
+      // now account 1 should have 1000 Tokens
+      expect(await partialRefund.balanceOf(_addr1)).to.equal(
+        ethers.utils.parseEther("1000")
+      );
+
+      // and the contract should have 1 ether
+      expect(await ethers.provider.getBalance(partialRefund.address)).to.equal(
+        ethers.utils.parseEther("1")
+      );
+    });
+
+    it("should not allow buying 0 tokens", async () => {
+      expect(await partialRefund.balanceOf(_addr1)).to.equal(
+        BigNumber.from("0")
+      );
+      expect(await ethers.provider.getBalance(partialRefund.address)).to.equal(
+        BigNumber.from("0")
+      );
+
+      // buy 0 tokens...
+      await expect(
+        partialRefund
+          .connect(addr1)
+          .buy({ value: ethers.utils.parseEther("0") })
+      ).to.be.revertedWith("Insufficient amount of ether sent!");
+    });
+
+    it("should allow the owner to withdraw", async () => {
+      // // buy 1000 Tokens by sending 1 ether
+      let tx = await partialRefund
+        .connect(addr1)
+        .buy({ value: ethers.utils.parseEther("1") });
+      await tx.wait();
+
+      // and the contract should have 1 ether
+      expect(await ethers.provider.getBalance(partialRefund.address)).to.equal(
+        ethers.utils.parseEther("1")
+      );
+
+      // now we send this 1 ether to the owner
+      // get the owner bal before withdraw
+
+      expect(await ethers.provider.getBalance(_deployer)).to.be.closeTo(
+        ethers.utils.parseEther("100"),
+        ethers.utils.parseEther("10")
+      );
+      const before = await ethers.provider.getBalance(_deployer);
+      console.log(before);
+
+      // // now withdraw from the contract to the owner
+      // tx = await partialRefund.withdraw();
+      // await tx.wait();
+
+      // // no more ether in the contract
+      // expect(await ethers.provider.getBalance(partialRefund.address)).to.equal(ethers.utils.parseEther('0'));
+
+      // // the owner has it
+      // expect(await ethers.provider.getBalance(_owner)).to.be.closeTo(ethers.utils.parseEther('1').add(before), ethers.utils.parseEther('0.01'));
+      // // sanity check
+      // expect(await ethers.provider.getBalance(_owner)).to.not.be.closeTo(ethers.utils.parseEther('2').add(before), ethers.utils.parseEther('0.01'));
+
+      // // gas is more than this, so check
+      // expect(await ethers.provider.getBalance(_owner)).to.not.be.closeTo(ethers.utils.parseEther('2').add(before), ethers.utils.parseEther('0.0000000001'));
     });
   });
 });
