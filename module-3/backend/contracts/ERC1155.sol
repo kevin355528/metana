b1feda74f2d0cccec53c695d0a02dc06253a7c03
kevin355528 <kevin901219@gmail.com>
2025-02-20 10:16:26 +0800

    Update module-3/backend/contracts/ERC1155.sol
    Co-authored-by: Dhruvin Parikh <29928916+dhruvinparikh@users.noreply.github.com>

--- module-3/backend/contracts/ERC1155.sol
+++ module-3/backend/contracts/ERC1155.sol
@@ -62,8 +62,6 @@ contract erc1155 is ERC1155, AccessControl {
     function tradeToken(uint256 tokenId, uint256 desiredToken, uint256 amount) public {
         if (!(tokenId >= 3 && tokenId <= 6))
             revert ERC1155__InvalidTokenForTrade();
-        if (!(desiredToken >= TOKEN_ID_0 && desiredToken <= TOKEN_ID_2))
-            revert ERC1155__InvalidTokenForTrade();
 
         _burn(msg.sender, tokenId, amount);
         mint(msg.sender, desiredToken, amount);
