e52b023328663dfb3df86cd01b2745c15a5870e9
kevin355528 <kevin901219@gmail.com>
2025-03-27 14:16:02 +0800

    Completed tasks for module-8

--- module-8/Advanced-NFT/hardhat.config.js
+++ module-8/Advanced-NFT/hardhat.config.js
@@ -1,23 +1,26 @@
-require('@nomiclabs/hardhat-waffle'); // Waffle plugin
-require('@nomiclabs/hardhat-ethers'); // Ethers plugin
-require('hardhat-gas-reporter'); // Gas Reporter
-require('solidity-coverage'); // Solidity Coverage
+require("@nomiclabs/hardhat-waffle"); // Waffle plugin
+require("@nomiclabs/hardhat-ethers"); // Ethers plugin
+require("hardhat-gas-reporter"); // Gas Reporter
+require("solidity-coverage"); // Solidity Coverage
 
 // You can add more plugins here if you need them
 
 module.exports = {
-	solidity: '0.8.20',
-	networks: {
-		// Define your network configurations here if needed
-		hardhat: {
-			// Configuration for the Hardhat network (if any specific settings required)
-		},
-	},
+  solidity: "0.8.20",
+  networks: {
+    // Define your network configurations here if needed
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/b64e582aca814a4f859e7461fc66f713",
+      accounts: [
+        "a5129758a0d28b9717a05c82c3f3da16e3cf3e185841ecc5b3ba0e73072e2d1d",
+      ],
+    },
+  },
 
-	gasReporter: {
-		// Gas reporter configuration
-		enabled: process.env.REPORT_GAS !== undefined,
-		currency: 'USD',
-	},
-	// Additional configurations can be added here
+  gasReporter: {
+    // Gas reporter configuration
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  // Additional configurations can be added here
 };
